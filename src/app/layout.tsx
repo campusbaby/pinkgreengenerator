989e558ca1fcdcde2303feae816207a15acd1cef
campusbaby <824377536@qq.com>
2025-09-07 07:58:09 +0800

    增加统计代码

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -84,6 +84,37 @@ export default function RootLayout({
             }),
           }}
         />
+        {process.env.NODE_ENV === 'production' && (
+          <>
+            {/* Google Analytics */}
+            <script
+              async
+              src="https://www.googletagmanager.com/gtag/js?id=G-0SEN47EDMY"
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', 'G-0SEN47EDMY');
+                `,
+              }}
+            />
+            {/* Microsoft Clarity */}
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  (function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                  })(window, document, "clarity", "script", "t6rf0mhpse");
+                `,
+              }}
+            />
+          </>
+        )}
       </head>
       <body className="antialiased">
         {children}
